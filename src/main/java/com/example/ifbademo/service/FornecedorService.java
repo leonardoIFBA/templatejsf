93bd8207b745dbd504c5eec8c136cee620b75328
leonardoIFBA <162935511+leonardoIFBA@users.noreply.github.com>
2025-12-02 09:54:04 -0300

    Add files via upload

--- src/main/java/com/example/ifbademo/service/FornecedorService.java
+++ src/main/java/com/example/ifbademo/service/FornecedorService.java
@@ -4,26 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.ifbademo.model.Fornecedor;
 import com.example.ifbademo.repository.FornecedorRepository;
 
 @Service
+@Transactional(readOnly = true)
 public class FornecedorService {
     @Autowired
     private FornecedorRepository repository;
 
+    @Transactional(readOnly = false)
     public void salvar(Fornecedor f){
         repository.save(f);
     }
 
-    public void excluir(Fornecedor f){
-        repository.delete(f);
+    @Transactional(readOnly = false)       
+    public boolean excluirPorId(Long id){
+        if (! fornecedorTemMovimentacao(id)){
+            repository.deleteById(id);
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public void excluirPorId(Long id){
-        repository.deleteById(id);
-    }
+    private boolean fornecedorTemMovimentacao(Long id){
+        return buscarPorId(id).getMovimentacoes().isEmpty();
+        //return repository.existsByMovimentacoes_Id(id);
+    } 
 
     public Fornecedor buscarPorId(Long id){
         return repository.findById(id).get();
